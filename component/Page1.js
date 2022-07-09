424f38cf5744723ed70e23c9521b592f6bfabd6b
kishor <patilkk99@gmail.com>
2022-07-09 13:36:00 +0530

    with passing values between screens

--- component/Page1.js
+++ component/Page1.js
@@ -1,8 +1,12 @@
 import { Button, Text, TextInput, View } from "react-native";
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 
 export default function Page1({ navigation }) {
   const [text, setText] = useState();
+
+  useEffect(() => {
+    console.log(text);
+  }, [text]);
 
   return (
     <View>
--- component/Page2.js
+++ component/Page2.js
@@ -1,11 +1,12 @@
 import { Button, Text, View } from "react-native";
 import React, { Component } from "react";
 
-export default function Page2({ data, navigation }) {
+export default function Page2({ route, navigation }) {
+  console.log(route.params.text);
   return (
     <View>
       <Text>Page2</Text>
-      <Text>{data.params}</Text>
+      <Text>{route.params.text}</Text>
       <Button onPress={() => navigation.navigate("Page1")} title="to page1" />
     </View>
   );
